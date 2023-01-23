e9d60cadea97f2799170fd57d671c95cc2fb71fd
Gerson <gerson02@gmail.com>
2023-01-23 14:42:01 -0500

    Sample test for ios camera

---
+++ TestCameraIOs/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace TestCameraIOs;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ TestCameraIOs/obj/Debug/net7.0-android/android/src/crc6426bc86c210e677f3/MainActivity.java
@@ -0,0 +1,48 @@
+package crc6426bc86c210e677f3;
+
+
+public class MainActivity
+	extends crc6488302ad6e9e4df1a.MauiAppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"";
+		mono.android.Runtime.register ("TestCameraIOs.MainActivity, TestCameraIOs", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class) {
+			mono.android.TypeManager.Activate ("TestCameraIOs.MainActivity, TestCameraIOs", "", this, new java.lang.Object[] {  });
+		}
+	}
+
+
+	public MainActivity (int p0)
+	{
+		super (p0);
+		if (getClass () == MainActivity.class) {
+			mono.android.TypeManager.Activate ("TestCameraIOs.MainActivity, TestCameraIOs", "System.Int32, System.Private.CoreLib", this, new java.lang.Object[] { p0 });
+		}
+	}
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
